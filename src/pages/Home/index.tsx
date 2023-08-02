939fd5a8b651e1face50b413f412fe7f94f5ac7c
WagnerRodrigues <felten19@gmail.com>
2023-08-02 20:20:18 -0300

    Feita estilização da aplicação, da page home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,21 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-     <header>
-      <h1>Github Api</h1>
-     </header>
+      <header>
+        <h3>Github Api</h3>
+      </header>
+
+      <main>
+        <h1>Desafio Github API</h1>
+        <p>DevSuperior - Escola de programação</p>
+        <div className="Link">
+          <a href="#">Começar</a>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
